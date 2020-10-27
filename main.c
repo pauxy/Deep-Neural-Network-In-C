1548432c33e4fcdea046f535bc8bd573d6d0bff5
Daryl Lim <daryl.limkz@gmail.com>
2020-10-27 15:49:25 +0800

    Refractored and fixed off by one error

--- main.c
+++ main.c
@@ -63,14 +63,14 @@ double meanAbsolutevalue(double** training, double activatedVal){
 
 
 double* backwardsPropagation(double* biasWeights, double activatedVal, double** training,double sumLR){
-    double* newBiasweights=malloc(DATA_COLUMNS+1*sizeof(double));
+    double* newBiasweights=malloc((DATA_COLUMNS+1)*sizeof(double));
     double ph = (exp(sumLR)/((1+exp(sumLR))*(1+exp(sumLR))));
     double biasTotal =0;
     for(int j = 0; j<DATA_COLUMNS; j++){
         double weightTotal=0;
         for(int i = 0; i<TRAINING_MAX; i++){
             double ph1 = activatedVal-training[i][DATA_COLUMNS-1];
-            //printf("%f %f\n",ph1,ph);
+            /* printf("%f %f\n",ph1,ph); */
             weightTotal += (ph*ph1*training[i][j]);
             if(j==0){
                 biasTotal += ph*ph1;
@@ -79,45 +79,47 @@ double* backwardsPropagation(double* biasWeights, double activatedVal, double** 
         *(newBiasweights+1+j)=*(biasWeights+1+j)-(weightTotal/TRAINING_MAX);
     }
     *newBiasweights=*(biasWeights)-biasTotal/TRAINING_MAX;
-    //printf("diff: %f\n",biasTotal);
+    /* printf("diff: %f\n",biasTotal); */
     return newBiasweights;
 }
 
 int main(){
-    double** data =openData("/Users/chuny/Downloads/fertility_Diagnosis_Data_Group1_4-1.txt");
-    double** training = malloc(TRAINING_MAX*sizeof(double*));
-    double** testing = malloc(TESTING_MAX*sizeof(double*));
-    training=data;
-    testing=data+90;
+    double** data = openData("dataset/fertility_Diagnosis_Data_Group1_4-1.txt");
+    double** training = (double**)malloc(TRAINING_MAX * sizeof(double*));
+    double** testing = (double**)malloc(TESTING_MAX * sizeof(double*));
+    training = data;
+    testing = data + 90;
     srand(time(NULL));
-    double* biasWeights = malloc(DATA_COLUMNS+1*sizeof(double));
-    *biasWeights=-1.5;
-    for (int i = 1;i<DATA_COLUMNS+1; i++){
-        *(biasWeights+i) = (double)rand()/(double)(RAND_MAX);
-        printf("Random Number %i:%f\n",i,*(biasWeights+i));
+    double* biasWeights = (double*)malloc((DATA_COLUMNS + 1) * sizeof(double));
+    *biasWeights = -1.5;
+    for (int i = 1; i < DATA_COLUMNS + 1; i++) {
+        *(biasWeights + i) = (double)rand() / (double)(RAND_MAX);
+        printf("Random Number %i:%f\n", i, *(biasWeights + i));
     }
-    int t=0;
-    double sumLR=0;
-    double activatedVal=0;
-    double maeVal=0;
-    do{
-        if(t>0){
-            biasWeights=backwardsPropagation(biasWeights,activatedVal,training,sumLR);
-        for (int i = 1;i<DATA_COLUMNS+1; i++){
-            //printf("Random Number %i:%f\n",i,*(biasWeights+i));
-            
+    int t = 0;
+    double sumLR = 0;
+    double activatedVal = 0;
+    double maeVal = 0;
+
+    do {
+        if (t > 0) {
+            biasWeights = backwardsPropagation(biasWeights, activatedVal, training, sumLR);
+            for (int i = 1;i<DATA_COLUMNS+1; i++){
+                /* printf("Random Number %i:%f\n", i, *(biasWeights + i)); */
+            }
         }
+        sumLR = linearRegression(training, biasWeights);
+        activatedVal = sigmoid(sumLR);
+        maeVal = meanAbsolutevalue(training,activatedVal);
+        t += 1;
+
+        if(t % 10000 == 0 || t < 10000){
+            printf("Sum of LR: %f\n",sumLR);
+            printf("Sigmoid value: %0.300f\n",activatedVal);
+            printf("MAE value: %f\n",maeVal);
+            printf("T value: %i\n",t);
         }
-        sumLR=linearRegression(training,biasWeights);
-        activatedVal=sigmoid(sumLR);
-        maeVal=meanAbsolutevalue(training,activatedVal);
-        t+=1;
-        if(t%10000==0||t<10000){
-        printf("Sum of LR: %f\n",sumLR);
-        printf("Sigmoid value: %0.300f\n",activatedVal);
-        printf("MAE value: %f\n",maeVal);
-        printf("T value: %i\n",t);
-        
-    }}while(maeVal>0.25);
+    } while (maeVal > 0.25);
+
     return 0;
 }
