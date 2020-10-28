50799c672e48f70b7953319cc962cecbd1e1b450
Pauxy <chunyu1399@outlook.com>
2020-10-28 22:52:49 +0800

    fixed bugs

--- main.c
+++ main.c
@@ -11,10 +11,10 @@ const int DATA_ROWS = 100;
 const double LEARNING_RATE = 0.05;
 
 double** openData(char*);
-double linearRegression(double**, double*);
-double sigmoid(double);
-double meanAbsoluteValue(double**, double);
-double* backwardsPropagation(double*, double, double**, double);
+double* linearRegression(double**, double*);
+double* sigmoid(double*);
+double meanAbsoluteValue(double**, double*);
+double* backwardsPropagation(double*, double*, double**, double*);
 
 double** openData(char* filename) {
     double* val = (double*)malloc(DATA_ROWS * DATA_ROWS * sizeof(double));
@@ -41,42 +41,44 @@ double** openData(char* filename) {
 }
 
 
-double linearRegression(double** data, double* biasWeights) {
-    double total = 0.0;
-    double bias = *biasWeights;
+double* linearRegression(double** data, double* biasWeights) {
+    double* lr = (double*)malloc(DATA_COLUMNS * sizeof(double));
     for (int i = 0; i < TRAINING_MAX; i++) {
         for (int j = 0; j < DATA_COLUMNS; j++) {
-            total += *(biasWeights + 1 + j) * data[i][j];
+            *(lr + j)= *(biasWeights + 1 + j) * data[i][j] + *biasWeights;
         }
-        total += bias;
     }
-    return total;
+    return lr;
 }
 
 
-double sigmoid(double sumLR) {
-    return 1.0 / (1.0 + exp(-sumLR));
+double* sigmoid(double* lr) {
+    double* activatedVal = (double*)malloc(DATA_COLUMNS * sizeof(double));
+    for(int i = 0; i<DATA_COLUMNS; i++){
+        *(activatedVal + i) = 1.0 / (1.0 + exp(- *(lr + i)));
+    }
+    return activatedVal;
 }
 
 
-double meanAbsoluteValue(double** training, double activatedVal) {
+double meanAbsoluteValue(double** training, double* activatedVal) {
     double total = 0.0;
     for (int i = 0; i < TRAINING_MAX; i++) {
-        total += fabs(activatedVal - training[i][DATA_COLUMNS - 1]);
+        total += fabs( *(activatedVal + i) - training[i][DATA_COLUMNS - 1]);
     }
     return total / TRAINING_MAX;
 }
 
 
-double* backwardsPropagation(double* biasWeights, double activatedVal,
-                             double** training, double sumLR) {
+double* backwardsPropagation(double* biasWeights, double* activatedVal,
+                             double** training, double* lr) {
     double* newBiasWeights = (double*)malloc((DATA_COLUMNS + 1) * sizeof(double));
-    double ph = exp(sumLR) / pow(1.0 + exp(sumLR), 2.0);
     double biasTotal = 0.0;
     for (int j = 0; j < DATA_COLUMNS; j++) {
         double weightTotal = 0.0;
         for (int i = 0; i < TRAINING_MAX; i++) {
-            double ph1 = activatedVal - training[i][DATA_COLUMNS - 1];
+            double ph = exp( *(lr + j)) / pow(1.0 + exp( *(lr + j)), 2.0);
+            double ph1 = *(activatedVal + j) - training[i][DATA_COLUMNS - 1];
             /* printf("%f %f\n", ph1, ph); */
             weightTotal += (ph * ph1 * training[i][j]);
             if (j == 0) {
@@ -99,35 +101,28 @@ int main() {
     testing = data + 90;
     srand(time(NULL));
     double* biasWeights = (double*)malloc((DATA_COLUMNS + 1) * sizeof(double));
-    *biasWeights = -1.5;
+    *biasWeights = 0;
     for (int i = 1; i < DATA_COLUMNS + 1; i++) {
         *(biasWeights + i) = (double)rand() / (double)RAND_MAX;
         printf("Random Number %i:%f\n", i, *(biasWeights + i));
     }
     int t = 0;
-    double sumLR = 0.0;
-    double activatedVal = 0.0;
+    double* lr =  (double*)malloc(DATA_COLUMNS * sizeof(double));
+    double* activatedVal = (double*)malloc(DATA_COLUMNS * sizeof(double));
     double maeVal = 0.0;
 
     do {
-        if (t > 0) {
-            biasWeights = backwardsPropagation(biasWeights, activatedVal, training, sumLR);
-            for (int i = 1; i < DATA_COLUMNS + 1; i++) {
-                /* printf("Random Number %i:%f\n", i, *(biasWeights + i)); */
-            }
-        }
-        sumLR = linearRegression(training, biasWeights);
-        activatedVal = sigmoid(sumLR);
+        lr = linearRegression(training, biasWeights);
+        activatedVal = sigmoid(lr);
         maeVal = meanAbsoluteValue(training, activatedVal);
         t += 1;
 
         if (t % 10000 == 0 || t < 10000) {
-            printf("Sum of LR: %f\n",sumLR);
-            printf("Sigmoid value: %0.300f\n",activatedVal);
             printf("MAE value: %f\n",maeVal);
             printf("T value: %i\n",t);
         }
-    } while (maeVal > 0.25);
+        biasWeights = backwardsPropagation(biasWeights, activatedVal, training, lr);
+    } while (maeVal > 0.15);
 
     return 0;
 }
